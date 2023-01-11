fc3c0a666b3ff6d575fcbb98ef7cd13cb3f0fb64
Danh <duongngocdanh6@gmail.com>
2023-01-11 11:27:06 +0700

    update method of repository

--- src/main/java/com/example/demo/repository/ProductRepository.java
+++ src/main/java/com/example/demo/repository/ProductRepository.java
@@ -14,8 +14,14 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
     // todo
     public Product findByName(String name);
 
-    @Query("select p from Product p where p.name like  :name%")
+    // Product ở đây là tên của entity
+    @Query("select p from Product p where p.name like :name%")
     public ArrayList<Product> findAllNameContain(@Param("name") String name);
 
     public List<Product> findByNameLike(String name);
+
+    // products ở đây là tên trong database
+//    ở đây sử dụng nativeQuery = true cho phép dử dụng mysql
+    @Query(value = "Select * from products where name = ':name%'", nativeQuery = true)
+    public ArrayList<Product> findDanh(@Param("name") String name);
 }
